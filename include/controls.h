b38cde3963869c0aa309997a35b39dd6f41bc6b7
James Andrews <cjand225@gmail.com>
2024-05-14 16:51:42 -0600

    Fixed issue with references for position calc function

--- include/controls.h
+++ include/controls.h
@@ -11,6 +11,6 @@ glm::mat4 getProjectionMatrix();
 glm::vec2 getMousePosition(GLFWwindow* window);
 
 glm::vec3 calculateDirection(const float& verticleAngle, const float& horizontalAngle);
-void calculatePosition(GLFWwindow* window, glm::vec3& position, const glm::vec3& direction, const glm::vec3& right, float& deltaTime, float& speed);
+void calculatePosition(GLFWwindow* window, glm::vec3& position, const glm::vec3& direction, const glm::vec3& right, const float& deltaTime, const float& speed);
 
 #endif // CONTROLS_H 
--- src/controls.cpp
+++ src/controls.cpp
@@ -31,7 +31,7 @@ glm::vec3 calculateDirection(const float& verticleAngle, const float& horizontal
     );
 }
 
-void calculatePosition(GLFWwindow* window, glm::vec3& position, const glm::vec3& direction, const glm::vec3& right, float& deltaTime, float& speed)
+void calculatePosition(GLFWwindow* window, glm::vec3& position, const glm::vec3& direction, const glm::vec3& right, const float& deltaTime, const float& speed)
 {
     // Move Forward
     if (glfwGetKey( window, GLFW_KEY_UP ) == GLFW_PRESS){
