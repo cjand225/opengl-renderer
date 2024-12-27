4e63dcc3ce90b3ebe0d1703aacf84b3147b93770
James Andrews <cjand225@gmail.com>
2024-12-26 21:08:02 -0500

    tests: fix hard coded system path

--- tests/src/fileloader_test.cpp
+++ tests/src/fileloader_test.cpp
@@ -7,18 +7,18 @@
 class FileLoaderTest : public ::testing::Test {
 protected:
     std::filesystem::path getAssetPath(const std::string& filename) {
-        return std::filesystem::current_path() / "assets" / "models" / "Goku" / filename;
+        return std::filesystem::current_path() / "assets" / filename;
     }
 };
 
 TEST_F(FileLoaderTest, LoadMTLFile) {
-    std::string testPath  = getAssetPath("Goku.mtl").string();
+    std::string testPath  = getAssetPath("models/Goku/Goku.mtl").string();
     auto        materials = loadMTLFile(testPath);
     EXPECT_FALSE(materials.empty());
 }
 
 TEST_F(FileLoaderTest, LoadOBJFile) {
-    std::string testPath = getAssetPath("Goku.obj").string();
+    std::string testPath = getAssetPath("models/Goku/Goku.obj").string();
     auto        objData  = loadFromOBJ(testPath);
     EXPECT_FALSE(objData.vertices.empty());
     EXPECT_FALSE(objData.faces.empty());
