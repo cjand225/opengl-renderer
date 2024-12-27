5482ca38a15174bb30ff3b7314db0667176e502a
James Andrews <cjand225@gmail.com>
2024-12-26 20:48:39 -0500

    feat: introduced basic unit testing

---
+++ tests/src/fileloader_test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+
+#include <filesystem>
+
+#include "FileLoader.h"
+
+class FileLoaderTest : public ::testing::Test {
+protected:
+    std::filesystem::path getAssetPath(const std::string& filename) {
+        return std::filesystem::current_path() / "assets" / "models" / "Goku" / filename;
+    }
+};
+
+TEST_F(FileLoaderTest, LoadMTLFile) {
+    std::string testPath  = getAssetPath("Goku.mtl").string();
+    auto        materials = loadMTLFile(testPath);
+    EXPECT_FALSE(materials.empty());
+}
+
+TEST_F(FileLoaderTest, LoadOBJFile) {
+    std::string testPath = getAssetPath("Goku.obj").string();
+    auto        objData  = loadFromOBJ(testPath);
+    EXPECT_FALSE(objData.vertices.empty());
+    EXPECT_FALSE(objData.faces.empty());
+}
